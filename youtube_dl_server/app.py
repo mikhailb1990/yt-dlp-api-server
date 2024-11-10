d1b4e7e51e8b68e6c05fc995e75f643d82554dde
Mikhail Bhuta <mikhail@dirtcube.xyz>
2024-11-10 15:42:26 +0530

    Update app.py

--- youtube_dl_server/app.py
+++ youtube_dl_server/app.py
@@ -29,9 +29,7 @@ def get_videos(url, extra_params):
     ydl_params = {
         'format': 'best',
         'cachedir': False,
-        'logger': current_app.logger.getChild('youtube-dl'),
-        'username': os.getenv("YT_USERNAME"),
-        'password': os.getenv("YT_PASSWORD")
+        'logger': current_app.logger.getChild('youtube-dl')
     }
     ydl_params.update(extra_params)
     ydl = SimpleYDL(ydl_params)
